223972b81bfb747726f637d149badc1e935e0fee
wealthydeveloper <idongesitutong@gmail.com>
2020-12-19 18:49:39 +0100

    modified the redirection

--- app/Http/Controllers/Auth/ConfirmPasswordController.php
+++ app/Http/Controllers/Auth/ConfirmPasswordController.php
@@ -26,7 +26,7 @@ class ConfirmPasswordController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
+    protected $redirectTo = '/admin/home';
 
     /**
      * Create a new controller instance.
