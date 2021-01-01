37a033ca567d10a73d7c7621734606b3cdbafc78
wealthydeveloper <idongesitutong@gmail.com>
2021-01-01 22:21:15 +0100

    added login & registered links

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -42,7 +42,10 @@
                         @guest
                            
                             @if (Route::has('register'))
-                                
+                            
+                            <li><a href="{{ route('login') }}">Login</a></li>
+                            <li><a href="{{ route('register') }}">Register</a></li>
+
                             @endif
                         @else
                             <li class="nav-item dropdown">
