adfe90eec1cf2bc645b7f17cadd9ae8e11b66f11
bethesida <idongesitutong@gmail.com>
2024-01-22 14:41:26 +0100

    fixed display post in descending order

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -27,9 +27,10 @@ class PostsController extends Controller
 
     public function index()
     {
-        return view('admin.posts.index')->with('posts', Post::all());
-                                        
-    } 
+        $posts = Post::orderBy('created_at', 'desc')->get();
+        return view('admin.posts.index')->with('posts', $posts);
+    }
+
 
     /**
      * Show the form for creating a new resource.
